e29e4b1286c43405028390c7a9c122ba5b389881
eaayemen <48118396+eaayemen@users.noreply.github.com>
2022-01-10 13:44:51 +0300

    Add files via upload

---
+++ github.py
@@ -0,0 +1,8 @@
+# Ask the user to enter their first name and
+#  then display the length of their name.
+#   upload program file to GitHub and past the link.
+
+first_name=input("enter your first name :")
+print(len(first_name))
+
+
